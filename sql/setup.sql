18a587aa53ca204969ad8c387e5b5a27afdcf320
Zack Lyon <zack.j.lyon@gmail.com>
2022-02-10 12:54:54 -0800

    Add github oauth with github route and users controller and model

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,8 +2,15 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
 DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE posts (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
   post VARCHAR(255)
-)
+);
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  username TEXT NOT NULL,
+  email TEXT
+);
